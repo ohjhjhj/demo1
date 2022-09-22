722f35ce592df0a20dbe9994bbab3f1541ab201e
ohjhjhj <ogmlwls@naver.com>
2022-09-22 14:24:14 +0900

    project start

---
+++ src/main/java/com/example/demo1/service/MemberService.java
@@ -0,0 +1,9 @@
+package com.example.demo1.service;
+
+import java.util.List;
+import com.example.demo1.dto.MemberDTO;
+
+public interface MemberService {
+	List<MemberDTO> findMemberList();
+	MemberDTO findMemberDetail(String id);
+}
---
+++ src/main/java/com/example/demo1/service/MemberServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo1.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo1.dto.MemberDTO;
+import com.example.demo1.mapper.MemberMapper;
+
+
+@Service
+public class MemberServiceImpl implements MemberService {
+	@Autowired
+	private MemberMapper memberMapper;
+
+	@Override
+	public List<MemberDTO> findMemberList() {
+		// TODO Auto-generated method stub
+		return memberMapper.selectMemberList();
+	}
+
+	@Override
+	public MemberDTO findMemberDetail(String id) {
+		// TODO Auto-generated method stub
+		return memberMapper.selectMemberDetail(id);
+	}
+}
